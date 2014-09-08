2dda5d32f66d1429d656621bff26dce51ced6753
Kamal Galrani <kamal1210@gmail.com>
2014-09-08 16:16:54 +0530

    guiOptionAdded

--- On-Board/ros_ws/src/map_generation/scripts/gz_delayed_launch.bash
+++ On-Board/ros_ws/src/map_generation/scripts/gz_delayed_launch.bash
@@ -2,4 +2,8 @@
 . ~/.bashrc
 @msi
 rosrun map_generation opencv_node &
+if [ $2 -ne 1 ]; then
 rosrun gazebo_ros gzserver worlds/$1.world --verbose
+else
+rosrun gazebo_ros gazebo worlds/$1.world --verbose
+fi
